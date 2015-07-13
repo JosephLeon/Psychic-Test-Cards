b2d789252fa4d70bdcb171ebd3e0258286b03765
Joseph Leon <joseph.leon.9@gmail.com>
2015-07-13 08:49:53 -0400

    Add label to calculate results into percentage, move around assignments.

--- PsychicTestCards/ViewController.swift
+++ PsychicTestCards/ViewController.swift
@@ -16,11 +16,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var card4: UIButton!
     @IBOutlet weak var card5: UIButton!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var resultsLabel: UILabel!
     
     var symbols = [String]()
     var score = 0
     var correctCard = 0
     var timesPlayed = 0
+    var resultsData = 0
     
     override func viewDidLoad() {
         
@@ -54,6 +56,12 @@ class ViewController: UIViewController {
         }
         
         ++timesPlayed
+        
+
+        if (timesPlayed > 4) {
+            resultsData = (score / timesPlayed) * 100
+            resultsLabel.text = "\(score) \(timesPlayed) \(resultsData)%"
+        }
         
         self.scoreLabel.text = "\(score) out of \(timesPlayed)"
         
